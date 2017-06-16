bbbc73918563006b23a0275c4ad59b7048753da9
Faye Salwin <faye.salwin@futureadvisor.com>
2017-06-16 10:51:37 -0700

    Windows compatibility updates

--- execwdve/main.go
+++ execwdve/main.go
@@ -2,31 +2,80 @@ package main
 
 import (
   "syscall"
+  "unsafe"
   "strings"
   "log"
   "os"
   "os/exec"
 )
 
+type Termios struct {
+	Iflag  uint64
+	Oflag  uint64
+	Cflag  uint64
+	Lflag  uint64
+	Cc     [20]byte
+	Ispeed uint64
+	Ospeed uint64
+}
+
+// TcSetAttr restores the terminal connected to the given file descriptor to a
+// previous state.
+func TcSetAttr(fd uintptr, termios *Termios) error {
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(setTermios), uintptr(unsafe.Pointer(termios))); err != 0 {
+		return err
+	}
+	return nil
+}
+
+// TcGetAttr retrieves the current terminal settings and returns it.
+func TcGetAttr(fd uintptr) (*Termios, error) {
+	var termios = &Termios{}
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, getTermios, uintptr(unsafe.Pointer(termios))); err != 0 {
+		return nil, err
+	}
+	return termios, nil
+}
+
+func stty_onlcr(fd uintptr) (*Termios, error) {
+	old, err := TcGetAttr(fd)
+	if err != nil {
+		return nil, err
+	}
+
+	new := *old
+	new.Oflag |= syscall.ONLCR
+
+	if err := TcSetAttr(fd, &new); err != nil {
+		return nil, err
+	}
+	return old, nil
+}
+
 // Trampoline used to be sufficient until I needed environment handling
 
 func main() {
-        env := os.Environ()
-        args := []string{}
+  env := os.Environ()
+  args := []string{}
 	workdir := ""
 	stage := 0
 	for _, arg := range os.Args[1:] {
-	  if stage == 0 && strings.Contains(arg, "=") {
-		env = append(env, arg)
-	  } else {
-		if stage == 0 {
-			workdir = arg
-			stage++
-		} else {
-			args = append(args, arg)
+    if strings.Contains(arg, "-onlcr") {
+      fd := uintptr(syscall.Stdout)
+      stty_onlcr(fd)
+    } else {
+			if stage == 0 && strings.Contains(arg, "=") {
+			env = append(env, arg)
+			} else {
+				if stage == 0 {
+					workdir = arg
+					stage++
+				} else {
+					args = append(args, arg)
+				}
+			}
 		}
-	  }
-	}
+  }
 	if len(args) == 0 {
 		log.Fatal("Usage: execwdve [<env>=<val>]... <workdir> <cmd> [<args>]...")
 	}
