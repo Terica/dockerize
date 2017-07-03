40d187d0f3c563b2ae6cae9a947b402fe2d72ee4
fayep <faye.salwin@opower.com>
2017-07-03 10:46:43 -0700

    Windowscompat (#1)
    * Windows compatibility
    
    * Better windows handling, update for new pinata
    
    * tests and bug fixes, return of bashspec.
    
    * fix typo in fail function in testing which would have prevented failure messages from showing properly

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "building execwdve"
+home=$HOME
+test -n "$USERPROFILE" && home=$USERPROFILE
 CTR=$(docker run -d -v ${PWD}:/go/src golang:latest go install execwdve)
 # wait for build
 while test -n "$(docker ps -qf id=${CTR})"; do sleep 1; docker ps -qf id=${CTR}; done
 docker logs ${CTR}
-docker cp ${CTR}:/go/bin/execwdve ${HOME}/bin
+mkdir -p ${home}/bin
+docker cp ${CTR}:/go/bin/execwdve ${home}/bin
 docker rm ${CTR}
--- test.sh
+++ test.sh
@@ -4,21 +4,86 @@ fail() {
 	exit 1
 }
 
+should() {
+	let testnumber=testnumber+1
+	testname=("$@")
+	testoutput="$(bash)"
+	result=$?
+        if [ "${testname[0]}" = "not" ]; then
+		test $result -eq 0 && return 1
+		return 0
+	fi
+	return $result
+}
+
+it() {
+	if "$@"; then
+          let testpass=testpass+1
+	else
+	  echo "it $@: FAILED"
+	  echo "$testoutput"
+	fi
+}
+
+# Are we already running in docker?
 grep -q docker /proc/$$/cgroup 2>/dev/null
 if [ $? -ne 0 ]; then
-	docker run -i --rm -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker:ro -v ${PWD}:${PWD} -w ${PWD} ubuntu:14.04 "$0" "$@"
+	# These tests will run in a standardized golang container
+	# which we then remove golang from - go figure.
+	docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker:ro -v ${PWD}:${PWD} -w ${PWD} golang:latest "$0" "$@"
 	exit
 fi
 uname -a
 test -S /var/run/docker.sock || fail "You need to pass in docker and the docker socket."
 version=$(docker version 2>&1) || fail -e "Docker is not functional in the container.\n$version"
-cp dockerize /usr/local/bin
-chmod +x /usr/local/bin/dockerize
-test -e /usr/local/bin/golang && fail "golang shouldn't exist in this container to start with. no cheating."
-dockerize install golang go || fail "dockerize golang go failed."
-ls -li /usr/local/bin
-test -e /usr/local/bin/golang || fail "we should have just created golang"
-test -L /usr/local/bin/go || fail "we should have created a symlink go too"
-[ $(ls -i /usr/local/bin/golang | cut -d' ' -f1) = $(ls -i /usr/local/bin/dockerize | cut -d' ' -f1) ] ||
-	fail "Dockerize and golang should reference the same inode."
-true
+set -o errexit
+eval $(ssh-agent) >& /dev/null
+# no cheating
+rm -rf /usr/local/go
+./build.sh >& /dev/null
+ssh-keygen -N "" -f ~/.ssh/id_rsa -C "testy@mctest.face" >& /dev/null
+ssh-add >& /dev/null
+rm -rf bin
+mkdir -p bin
+mv ~/bin/execwdve bin/
+export HOME=${PWD}
+export PATH=~/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# Things should look ok from here
+cp dockerize ~/bin
+chmod +x ~/bin/dockerize
+
+it should not already have golang installed << EOT
+  test -e /usr/local/bin/golang
+EOT
+
+it should return successfully when installing go << EOT
+  dockerize install golang go
+EOT
+
+it should have created golang hardlink << EOT
+  test -e bin/golang
+EOT
+
+it should have created a symlink to go <<EOT
+  test -L bin/go
+EOT
+
+it should have the same inode for containers <<EOT
+  [ "$(ls -i bin/golang | cut -d' ' -f1)" = "$(ls -i bin/dockerize | cut -d' ' -f1)" ]
+EOT
+
+it should be running the correct binary <<EOT
+  test "$(which go)" = "$PWD/bin/go"
+EOT
+
+it should run go from a container <<EOT
+  go version | grep -q go.*amd64
+EOT
+
+rm -rf bin
+echo
+test "$testpass" -gt 0 && echo -n "$testpass/$testnumber tests passed"
+test "$testpass" -eq "$testnumber" && echo -n "!"
+result=$?
+echo
+exit $result
