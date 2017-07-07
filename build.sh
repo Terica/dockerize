dadf8a00207c53472beedb8361231a4c3bd90246
Faye Salwin <faye.salwin@futureadvisor.com>
2017-07-07 09:47:11 -0700

    having moved go code to a subdirectory, build.sh needs to change

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "building execwdve"
 home=$HOME
 test -n "$USERPROFILE" && home=$USERPROFILE
-CTR=$(docker run -d -v ${PWD}:/go/src golang:latest go install execwdve)
+CTR=$(docker run -d -v ${PWD}/go:/go/src golang:latest go install execwdve)
 # wait for build
 while test -n "$(docker ps -qf id=${CTR})"; do sleep 1; docker ps -qf id=${CTR}; done
 docker logs ${CTR}
